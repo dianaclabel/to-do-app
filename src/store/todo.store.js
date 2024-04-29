5c35e6a740b4e8598d7797c2d5d285cfc068454a
Diana Clabel <dianaclabel08@gmail.com>
2024-04-29 15:53:59 -0500

    feat: Renderizar listado

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -2,8 +2,8 @@ import { Todo } from "../todos/models/todo.model";
 
 const Filters = {
   All: "all",
-  completed: "Completed",
-  pending: "pending",
+  Completed: "Completed",
+  Pending: "pending",
 };
 
 const state = {
@@ -11,6 +11,7 @@ const state = {
     new Todo("Piedra del alma"),
     new Todo("Piedra del infinito"),
     new Todo("Piedra del tiempo"),
+    new Todo("Piedra de realidad"),
   ],
   filter: Filters.All,
 };
@@ -24,13 +25,27 @@ const loadStore = () => {
   throw new Error("Not implemented");
 };
 
+const getTodos = (filter = Filters.All) => {
+  switch (filter) {
+    case Filters.All:
+      return [...state.todos];
+    case Filters.Completed:
+      return state.todos.filter((todo) => todo.done);
+    case Filters.Pending:
+      return state.todos.filter((todo) => !todo.done);
+    default:
+      throw new Error(`Option ${filter} is not valid`);
+  }
+};
+
 /**
  *
  * @param {String} description
  */
 
 const addTodo = (description) => {
-  throw new Error("Not implemented");
+  if (!description) throw new Error("Not implemented");
+  state.todos.push(new Todo(description));
 };
 
 /**
@@ -38,32 +53,45 @@ const addTodo = (description) => {
  * @param {String} todoId
  */
 const toggleTodo = (todoId) => {
-  throw new Error("Not implemented");
+  state.todos = state.todos.map((todo) => {
+    if (todo.id === todoId) {
+      todo.done = !todo.done;
+    }
+
+    return todo;
+  });
 };
 
-const deleteTodo = () => {
-  throw new Error("Not implemented");
+const deleteTodo = (todoId) => {
+  state.todos = state.todos.filter((todo) => todo.id != todoId);
 };
 
 const deleteCompleted = () => {
-  throw new Error("Not implemented");
+  state.todos = state.todos.filter((todo) => todo.done);
 };
 
+/**
+ *
+ * @param {Filters} newFilter
+ */
+
 const setFilter = (newFilter = Filters.All) => {
-  throw new Error("Not implemented");
+  // if (Object.keys(Filters).includes(newFilter))
+  state.filter = newFilter;
 };
 
 const getCurrentFilter = () => {
-  throw new Error("Not implemented");
+  return state.filter;
 };
 
 export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
+  getTodos,
   initStore,
   loadStore,
-  addTodo,
-  toggleTodo,
-  deleteTodo,
-  deleteCompleted,
   setFilter,
-  getCurrentFilter,
+  toggleTodo,
 };
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,15 +1,25 @@
-import html from "./app.html?raw"; // Nos permite importar un archivo HTML, el HTML se convierte en string.
-// HTML podemos inyectarlo en el JS
+import html from "./app.html?raw"; // Nos permite importar un archivo HTML, el HTML se convierte en string.HTML podemos inyectarlo en el JS
+import todoStore from "../store/todo.store";
+import { renderTodos } from "./use-cases";
 
+const ElementIDs = {
+  TodoList: ".todo-list",
+};
 /**
  *
  * @param { String }elementId
  */
 
 export const App = (elementId) => {
+  const displayTodos = () => {
+    const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+    renderTodos(ElementIDs.TodoList, todos);
+  };
+
   (() => {
     const app = document.createElement("div");
     app.innerHTML = html;
     document.querySelector(elementId).append(app);
+    displayTodos();
   })();
 };
