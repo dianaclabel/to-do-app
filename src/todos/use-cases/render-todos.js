f2633067e3f9f3391c108fcc223d7ae2fb8697d4
Diana Clabel <dianaclabel08@gmail.com>
2024-04-30 23:52:05 -0500

    feat: se renderiza las tareas

--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -1,5 +1,7 @@
 import { Todo } from "../models/todo.model";
 import { createTodoHTML } from "./";
+
+let element;
 /**
  *
  * @param {string} elementId
@@ -7,8 +9,11 @@ import { createTodoHTML } from "./";
  */
 
 export const renderTodos = (elementId, todos = []) => {
-  // TODO referencia
-  const element = document.querySelector(elementId);
+  if (!element) element = document.querySelector(elementId);
+
+  if (!element) throw new Error(`Element ${elementId} not found`);
+
+  element.innerHTML = "";
 
   todos.forEach((todo) => {
     element.append(createTodoHTML(todo));
