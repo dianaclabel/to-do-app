4efa6339e94fe83555dda8f0183d9a9137243f95
Diana Clabel <dianaclabel08@gmail.com>
2024-05-02 21:31:07 -0500

    Se agrega un nuevo todo a la lista

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,9 +1,11 @@
+// Esto solo funciona vite https://vitejs.dev/guide/assets.html#importing-asset-as-string
 import html from "./app.html?raw"; // Nos permite importar un archivo HTML, el HTML se convierte en string.HTML podemos inyectarlo en el JS
 import todoStore from "../store/todo.store";
 import { renderTodos } from "./use-cases";
 
 const ElementIDs = {
   TodoList: ".todo-list",
+  NewTodoInput: "#new-todo-input",
 };
 /**
  *
@@ -22,4 +24,18 @@ export const App = (elementId) => {
     document.querySelector(elementId).append(app);
     displayTodos();
   })();
+
+  //Referencias HTML
+  const newDescriptionInput = document.querySelector(ElementIDs.NewTodoInput);
+
+  //Listeners
+  newDescriptionInput.addEventListener("keyup", (event) => {
+    console.log(event);
+    if (event.keyCode !== 13) return;
+    if (event.target.value.trim().length === 0) return;
+
+    todoStore.addTodo(event.target.value);
+    displayTodos();
+    event.target.value = "";
+  });
 };
